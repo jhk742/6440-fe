16679e8b2ab39375f1affe2d1f7d375096f90d48
Jin Hyeong Kim <78355498+jhk742@users.noreply.github.com>
2025-04-19 17:10:20 -0400

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -23,6 +23,10 @@ function App() {
   const [question, setQuestion] = useState("");
   const [patientId, setPatientId] = useState("");
   const [messages, setMessages] = useState([]);
+  const [patientName, setPatientName] = useState("First Last");
+  const [patientGender, setPatientGender] = useState("")
+  const [patientDOB, setPatientDOB] = useState("")
+  const [patientAddr, setPatientAddr] = useState("")
 
   return (
     <div style={{ width: 1920, height: 1500, left: 0, top: 0, position: 'absolute', background: '#F0FFFF' }}>
@@ -53,13 +57,25 @@ function App() {
                   <div style={{ width: 40.72, height: 40.72, padding: 5, display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
                     <img src={searchIcon} alt="search" style={{ width: 30.54, height: 30.54 }} />
                   </div>
-                  <input
-                    type="text"
-                    placeholder="Enter patient ID"
-                    value={patientId}
-                    onChange={e => setPatientId(e.target.value)}
-                    style={{ width: 300, height: 30, fontSize: 16, paddingLeft: 10 }}
-                  />
+                  <Select
+                      options={[
+                        { value: "Adrianne466_Jonnie215_Glover433_99d3b9b2-46c1-ef9e-da70-81ac3d365f52", label: "Adrianne Jonnie Glover", gender: "female", dob: "1939-06-16", address: "203 Marvin Parade Suite 80, North Westport, MA, US, 00000" },
+                        { value: "Ada662_Sari509_Balistreri607_dbc4a3f7-9c69-4435-3ce3-4e1988ab6b91", label: "Ada Sari Balistreri", gender: "female", dob: "1959-08-01", address: "315 Simonis Parade, Somerville, MA, USA, 02138" },
+                        { value: "Aleen595_Alanna27_Rogahn59_5358446e-e631-c640-5880-c6cf99dc8bed", label: "Aleen Alanna Rogahn", gender: "female", dob: "1973-10-11", address: "949 Okuneva Skyway Suite 61, Worcester, MA, US, 01604" },
+                        { value: "Aleisha941_Treutel973_2d799deb-df07-0c4a-8692-42cce7595251", label: "Aleisha Treutel", gender: "female", dob: "2006-02-13", address: "1095 Will Ville, Springfield, MA, US, 01108" },
+                        { value: "Zenia843_Genesis248_Weimann465_655baba7-47ed-22ac-2093-1196ebb44928", label: "Zenia Genesis Weimann", gender: "female", dob: "1927-03-20", address: "161 Hammes Union Unit 6, West Tisbury, MA, US, 00000" },
+                      ]}
+                      onChange={(e) => {
+                        setPatientId(e.value)
+                        setPatientName(e.label)
+                        setPatientGender(e.gender)
+                        setPatientDOB(e.dob)
+                        setPatientAddr(e.address)
+                      }}
+                      placeholder="Select a patient..."
+                      styles={{ container: (base) => ({ ...base, width: 300 }) }}
+                    />
+
                 </div>
                 <button
                   onClick={() => {
@@ -139,12 +155,12 @@ function App() {
               </div>
               <div style={{ display: 'flex', flexDirection: 'column', gap: 36 }}>
                 <div style={{ color: '#828282', fontSize: 36, fontFamily: 'Inter', fontWeight: 400 }}>
-                  First Last
+                  {patientName}
                 </div>
                 <div style={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
-                  <div style={{ color: '#828282', fontSize: 24 }}>Gender</div>
-                  <div style={{ color: '#828282', fontSize: 24 }}>DOB</div>
-                  <div style={{ color: '#828282', fontSize: 24 }}>Address</div>
+                  <div style={{ color: '#828282', fontSize: 24 }}>Gender: {patientGender}</div>
+                  <div style={{ color: '#828282', fontSize: 24 }}>DOB: {patientDOB}</div>
+                  <div style={{ color: '#828282', fontSize: 24 }}>Address: {patientAddr}</div>
                 </div>
               </div>
             </div>
